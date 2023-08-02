6a1b2ee0145501fe387aab5a77e8b156ffb7dbae
Brandt Redd <brandt@redd.org>
2023-08-02 17:51:50 -0600

    Change commandline syntax to be more universal.

--- Program.cs
+++ Program.cs
@@ -14,24 +14,26 @@ namespace CodeBit
   CodeBit <Action> [arguments...]
 
 Actions:
-  Validate <filename>
+  Validate [-file] <filename>
     Validate the codebit metadata in the designated file.
 
-  ValidateByUrl <url>
+  Validate -url <url>
     Validate a codebit at the specified URL.
 
-  ValidateByName <codeBitName> [-v <version>]
+  Validate -name <codeBitName> [-v <version>]
     Look up in a directory by name and validate.
 
-  ValidateDirectory <domainName>
+  Validate -dir <domainName>
     Validate the published directory for a particular domain name.
 
   GetVersion
     Report the version of this codebit tool.
 ";
 
+
         static Action? s_operation;
         static string s_target = string.Empty;
+        static TargetType s_targetType = TargetType.Unknown;
         static SemVer? s_version = null;
 
         static void Main(string[] args)
@@ -50,7 +52,7 @@ Actions:
 
         static void ParseCommandLine()
         {
-            bool getTarget = false;
+            TargetType defaultTargetType = TargetType.Unknown;
 
             var cl = new CommandLineLexer();
             // Get the command
@@ -58,22 +60,7 @@ Actions:
             {
                 case "validate":
                     s_operation = Validate;
-                    getTarget = true;
-                    break;
-
-                case "validatebyurl":
-                    s_operation = ValidateByUrl;
-                    getTarget = true;
-                    break;
-
-                case "validatebyname":
-                    s_operation = ValidateByName;
-                    getTarget = true;
-                    break;
-
-                case "validatedirectory":
-                    s_operation = ValidateDirectory;
-                    getTarget = true;
+                    defaultTargetType = TargetType.Filename;
                     break;
 
                 case "getversion":
@@ -103,51 +90,79 @@ Actions:
                     return;
             }
 
-            if (getTarget)
-            {
-                var target = cl.ReadNextArg();
-                if (target == null)
-                {
-                    cl.ThrowValueError("Expected target for operation.");
-                }
-                s_target = target;
-            }
-
             while (cl.MoveNext())
             {
-                cl.ThrowIfNotOption();
-                switch (cl.Current.ToLowerInvariant())
+                if (cl.IsOption)
                 {
-                    case "-v":
-                    case "-version":
-                        s_version = SemVer.ParseForSearch(cl.ReadNextValue());
-                        break;
+                    switch (cl.Current.ToLowerInvariant())
+                    {
+                        case "-file":
+                            if (s_targetType != TargetType.Unknown) continue; // Take the first target found
+                            s_target = cl.ReadNextValue();
+                            s_targetType = TargetType.Filename;
+                            break;
 
-                    default:
-                        cl.ThrowUnexpectedArgError();
-                        break;
+                        case "-url":
+                            if (s_targetType != TargetType.Unknown) continue; // Take the first target found
+                            s_target = cl.ReadNextValue();
+                            s_targetType = TargetType.CodebitUrl;
+                            break;
+
+                        case "-name":
+                            if (s_targetType != TargetType.Unknown) continue; // Take the first target found
+                            s_target = cl.ReadNextValue();
+                            s_targetType = TargetType.CodebitName;
+                            break;
+
+                        case "-dir":
+                            if (s_targetType != TargetType.Unknown) continue; // Take the first target found
+                            s_target = cl.ReadNextValue();
+                            s_targetType = TargetType.DirectoryDomain;
+                            break;
+
+                        case "-v":
+                        case "-version":
+                            s_version = SemVer.ParseForSearch(cl.ReadNextValue());
+                            break;
+
+                        default:
+                            cl.ThrowUnexpectedArgError();
+                            break;
+                    }
+                }
+                else
+                {
+                    if (s_targetType != TargetType.Unknown) continue; // Take the first target found
+                    s_target = cl.Current;
+                    s_targetType = defaultTargetType;
                 }
             }
         }
 
         static void Validate()
         {
-            CodeBitValidator.ValidateFile(s_target);
-        }
+            switch (s_targetType)
+            {
+                case TargetType.Filename:
+                    CodeBitValidator.ValidateFile(s_target);
+                    break;
 
-        static void ValidateByUrl()
-        {
-            CodeBitValidator.ValidatePublishedCodebit(s_target);
-        }
+                case TargetType.CodebitUrl:
+                    CodeBitValidator.ValidatePublishedCodebit(s_target);
+                    break;
 
-        static void ValidateByName()
-        {
-            CodeBitValidator.ValidateByName(s_target, s_version);
-        }
+                case TargetType.CodebitName:
+                    CodeBitValidator.ValidateByName(s_target, s_version);
+                    break;
 
-        static void ValidateDirectory()
-        {
-            CodeBitValidator.ValidateDirectory(s_target);
+                case TargetType.DirectoryDomain:
+                    CodeBitValidator.ValidateDirectory(s_target);
+                    break;
+
+                default:
+                    Console.Error.WriteLine("No target specified for Validate command.");
+                    break;
+            }
         }
 
         static void GetSyntax()
@@ -159,5 +174,15 @@ Actions:
         {
             Console.WriteLine(new FileMeta.AssemblyMetadata(typeof(Program)).Summary);
         }
+    } // Class Program
+
+    enum TargetType
+    {
+        Unknown,
+        Filename,
+        CodebitUrl,
+        CodebitName,
+        DirectoryDomain
     }
+
 }
